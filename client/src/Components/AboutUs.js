aae6cc03be86a28fa1233f068876e5c2168ef09e
diego <guatemaladiegojose@gmail.com>
2023-10-17 21:18:26 -0400

    added css for about us and orgized about us too

--- client/src/Components/AboutUs.js
+++ client/src/Components/AboutUs.js
@@ -5,7 +5,7 @@ function AboutUs(){
 
     return(
 
-            <p style={{textAlignVertical: "center",textAlign: "center"}}> InstaCake is an app designed to help bakeries to show their product
+            <p2 className= "aboutUs"> InstaCake is an app designed to help bakeries to show their product
                 to the world and collect reviews from actual users that have tasted 
                 their product or simply feel like adding a review. It's a free that 
                 is intentionally made to show cake designs as well. Either a user
@@ -13,7 +13,7 @@ function AboutUs(){
                 for visiting. InstakeCake is not responsible for bad comments, rather 
                 it intents to be a constructive opinion app for our society. We encourage 
                 out puplic to be as respecful as possible. Enjoy!
-            </p>
+            </p2>
     )
 }
 
